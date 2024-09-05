f763c6277a1491a4a1882797b404cf07b46f7a99
seyiadel <seyiadel03@gmail.com>
2024-09-05 11:38:43 +0100

    Commit de init, host test

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+
+const app = express()
+
+let PORT = 7000
+
+app.use(express.json())
+
+app.post("/webhook-event", (req, res)=>{
+    let payload = req.body
+
+    console.log(payload)
+    console.log("Trying...")
+    console.log(JSON.stringify(payload))
+
+    if(req.body.event.type == "app_mention"){
+        console.log("Event Confirmed..")
+    }
+    
+    return res.status(200).json(payload.challenge)
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running ${PORT}`)
+})
